c57ccebdc0c4dc55babf1ce7cc12a875d84b278a
takashi1124 <76987521+takashi1124@users.noreply.github.com>
2021-02-09 21:21:19 +0900

    商品出品機能の修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,13 +9,6 @@ RSpec.describe Item, type: :model do
       it '必須項目が全て正しく存在すれば出品できる' do
         expect(@item).to be_valid
       end
-      it '販売価格が300〜9,999,999であれば出品できる' do
-        expect(@item).to be_valid
-      end
-      it '販売価格が半角数字で入力されていれば出品できる' do
-        @item.price = '111111'
-        expect(@item).to be_valid
-      end
     end
     context '新規商品出品ができない' do
       it '商品画像が空では出品できない' do
@@ -79,12 +72,14 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it '販売価格が10,000,000円以上では出品できない' do
-        @item.price >= 10000000
+        @item.price = 10000000
         @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
       it '販売価格が299円以下では出品できない' do
-        @item.price <= 299
+        @item.price = 299
         @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
     end
   end
