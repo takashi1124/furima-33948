b33bee7dab7783a8221a5a3228b0fe1e17816eba
takashi1124 <76987521+takashi1124@users.noreply.github.com>
2021-02-19 15:34:07 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,13 +50,11 @@ class ItemsController < ApplicationController
   end
 
   def editor
-    unless @item.user_id == current_user.id
-      if @item.order
-        redirect_to root_path
-      else
-        @item.user_id == current_user.id
-        redirect_to root_path
-      end
+    if @item.order
+      redirect_to root_path
+    elsif
+      @item.user_id != current_user.id
+      redirect_to root_path
     end
   end
 
